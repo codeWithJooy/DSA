11c8d5884db1ee1c5a2637ba8bf2626e13489073
codeWithJooy <enigma.abhi.ty@gmail.com>
2022-08-28 07:18:17 +0530

    Level Order Traversal Added

--- Trees/Traversals/Traversal.py
+++ Trees/Traversals/Traversal.py
@@ -14,8 +14,27 @@ class Tree:
         print("Data is ",root.data)
         self.preorder(root.left)
         self.preorder(root.right)
-      
 
+    def levelOrder(self,root):
+        if(root==None):
+            return
+
+        #get a queue(list)
+        que=[]
+        que.append(root)
+
+        while len(que)>0:
+            node=que.pop(0)
+
+            #print the data
+            print(node.data)  
+
+            #Add Child Node
+            if(node.left!=None):
+                que.append(node.left)
+            if(node.right!=None):        
+                que.append(node.right)
+                
 root=Tree(10)  
 root.left=Tree(20)
 root.right=Tree(30)
@@ -24,4 +43,5 @@ root.left.right=Tree(50)
 root.right.left=Tree(60)
 root.right.right=Tree(70)
 
-root.preorder(root);      
+root.preorder(root)    
+root.levelOrder(root)
