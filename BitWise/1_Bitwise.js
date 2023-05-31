fc33300971e8959d3d310918f194aef7e6cfd1e5
Abhishek <enigma.abhi.ty@gmail.com>
2023-05-31 09:40:45 +0530

    Bitwise Not Added

--- BitWise/1_Bitwise.js
+++ BitWise/1_Bitwise.js
@@ -10,10 +10,17 @@ const Xor = (num1, num2) => {
   return num1 ^ num2;
 };
 
+const Not = (num) => {
+  return ~num;
+};
+
 num1 = 12;
 num2 = 25;
+num = 2;
 console.log(`Bitwise And of ${num1} and ${num2} is ${And(num1, num2)}`);
 
 console.log(`Bitwise OR of ${num1} and ${num2} is ${OR(num1, num2)}`);
 
 console.log(`Bitwise Xor of ${num1} and ${num2} is ${Xor(num1, num2)}`);
+
+console.log(`Bitwise Not of ${num} is ${Not(num)}`);
