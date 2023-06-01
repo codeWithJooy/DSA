b0d015f4bb32785dc52eb889a8b420a9befcbe99
Abhishek <enigma.abhi.ty@gmail.com>
2023-06-01 06:09:46 +0530

    Bitwise in Js

---
+++ BitWise/2_IfBitSet.js
@@ -0,0 +1,6 @@
+let num = 5; //number to check.Represented as 101
+let k = 1; // Check if Kth bit is set.Indexing starts from 0 from LSB
+
+console.log(`Check of ${k}th bit is set in ${num} using left Shift is`);
+
+console.log(`Check of ${k}th bit is set in ${num} using Right Shift is`);
